8a440900edcf9089345cebf3dd396cadbcbe7cb9
MINH DUC NGUYEN <mimiicteam@gmail.com>
2022-06-28 11:07:37 +0700

    Create Project

---
+++ AnalyticsPageUI/AnalyticsPageUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnalyticsPageUIApp.swift
+//  AnalyticsPageUI
+//
+//  Created by MINH DUC NGUYEN on 28/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct AnalyticsPageUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
